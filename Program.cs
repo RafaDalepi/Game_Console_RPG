53c2660fba54620c244895647310a62f97be3f6f
Rafael Dalepiane <rafadalepi@gmail.com>
2021-09-13 21:36:51 -0300

    Corrections

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace _2
     }
     
     class Program
-    {
+{
         static void Main(string[] args)
         {      
             int vida;
@@ -24,20 +24,25 @@ namespace _2
                 }
             for (int round = 1; vida > 0; round++)
             {
+                Console.WriteLine("------------------");
                 Console.WriteLine("Round: "+ round);
                 string _walk;
                 Console.WriteLine("Choose action: ");
                 _walk = Console.ReadLine();                       
-                if(_walk == "Walk"){
-            
+                if(_walk == "Drink pot"){
+                        Console.WriteLine("You feel revigorated!");
+                        vida++;
+                        Console.WriteLine("Your HP is: "+ vida);
+                }
+                if(_walk == "walk"){            
                 Random numGen = new Random();
                
                 int enc = numGen.Next(1, 3);
                 int dist = numGen.Next(10, 20);
                 int pot;
                 pot=0;
-                    if(enc==1){
-                        
+                    
+                    if(enc==1){                        
                         int gn = numGen.Next(1,2);
                             if(gn == 1){
                                 pot++;
@@ -46,23 +51,21 @@ namespace _2
                                 Console.WriteLine("you have now "+ pot +" healing potions");
                         }else if(gn == 2){
                             Console.WriteLine("You walks "+ dist + " meters");
+                        }else if(enc==2){
+                            Console.WriteLine("You found a skeleton");
+                            vida--;      
+                            Console.WriteLine("Your HP is "+ vida);          
+                        }else if(enc==3){
+                            Console.WriteLine("You found a goblin");
+                            vida --;
+                            vida --;
+                            Console.WriteLine("Your HP is "+ vida);
+                        }else{
+                        Console.WriteLine("Unexpected Comand");
                         }
-
-                    }else if(enc==2){
-                        Console.WriteLine("You found a skeleton");
-                        vida--;      
-                        Console.WriteLine("Your HP is "+ vida);          
-                    }else if(enc==3){
-                        Console.WriteLine("You found a goblin");
-                        vida --;
-                        Console.WriteLine("Your HP is "+ vida);
-                    }else if(_walk == "Drink pot"){
-                        Console.WriteLine("You feel revigorated!");
-                        vida++;
-                        Console.WriteLine("Your HP is: "+ vida);
-                    } 
+                    }
                 }
             }
-        }
+        }   
     }
 }
