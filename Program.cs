c58e14ea21a92c13a3edce4a2742d3c4dd6f1882
Rafael Dalepiane <rafadalepi@gmail.com>
2021-09-30 20:22:32 -0300

    Bugfixes

--- Program.cs
+++ Program.cs
@@ -31,25 +31,24 @@ namespace _2
                     }else{
                         Console.WriteLine("you have no healing potions left");
                     }
-                }else if(_walk == "walk"){
+                }else if(_walk == "walk" ){
                     int dist = numGen.Next(1, 100);                
-                    if(dist >= 30){
+                    if(dist >= 60){
                         pot++;
                         Console.WriteLine("You have walked "+ dist + " meters\nYou found a healing pot\nYou have now "+ pot +" healing potions ");
                     }else if(dist <= 29){
                         Console.WriteLine("You have walked "+ dist + " meters");
                     } 
-                }else if(_walk == "fight"){        
+                }else if(_walk == "fight" ){        
                 int enc = numGen.Next(1, 3);
                 string action;                
                     Console.WriteLine("You looked aroud and found an skeleton in the distance");
-                        for (int esk = 30; esk > 0;)
-                        {
+                        for (int esk = 30; esk > 0;){
                         int atk =  numGen.Next(1, 20);
                         int eskAtk = numGen.Next(1, 30);
                         Console.WriteLine("What do you want to do? ");
                         action = Console.ReadLine();                        
-                        if(action == "attack"){
+                        if(action == "attack" || vida > 0 ){
                                 if(atk <=10 ){
                                     Console.WriteLine("Your attack is weak \nYour attack causesd "+ atk + " of damage");
                                     esk = esk - atk;
@@ -57,9 +56,8 @@ namespace _2
                                     Console.WriteLine("Your attack is strong \nYour attack causes " + atk + " of damage\nThe skeleton have now " + esk + " of hp");
                                     esk = esk - atk; 
                                 }
-                                    vida = vida - eskAtk;
-                                    Console.WriteLine("The skeleton attaked you, causing "+ eskAtk + " of damage\nYour HP is now "+ vida);                                
-                                
+                                vida = vida - eskAtk;
+                                Console.WriteLine("The skeleton attaked you, causing "+ eskAtk + " of damage\nYour HP is now "+ vida);                                
                             }else if(action == "heal"){
                                 if(pot < 1){
                                     Console.WriteLine("You have no healing potions left");
@@ -71,12 +69,11 @@ namespace _2
                             }else{
                                 Console.WriteLine("Unexpected Comand");
                             }
-                        }             
-                    }
-                    if(vida == 0){
-                        Console.WriteLine("Game over");
-                        Console.ReadKey();
+                        }
+                    }             
                 }
+                if(vida == 0){
+                    Console.WriteLine("Game over");
             }
             Console.ReadKey();
             Console.Clear();   
